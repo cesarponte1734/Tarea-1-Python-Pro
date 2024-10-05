26d6a64671ae92b9652b1bddae0f5667524aa896
cesarponte1734 <cesar9pg@gmail.com>
2024-10-05 17:52:14 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+traduccion = {
+    "JUICIOSO" : "persona responsable o que cumple con sus deberes"
+    "CANGULL":"maiz pira o palomitas de maiz"
+    "AGUACATE":"es una fruta tambien conocida como palta"
+}
+
+palabra = input("ingresa una palabra en mayusculas, que quieras traducir: ")
+
+if palabra in traduccion.key
+    print("su significado es :", traduccion[palabra])
+else:
+    print("la palabra no existe")
